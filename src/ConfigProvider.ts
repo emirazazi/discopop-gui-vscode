9facfaef4d0dd9a8259b6e1b2c92ab7e742b7e9a
Emir Azazi <emir.azazi@gmx.de>
2022-06-16 11:50:46 +0200

    update readme

--- src/ConfigProvider.ts
+++ src/ConfigProvider.ts
@@ -5,11 +5,11 @@ export class ConfigProvider {
 
     public static workspacePath: string = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
-    public static discopopPath: string = vscode.workspace.getConfiguration("discopop").get("path");
+    public static discopopPath: string = vscode.workspace.getConfiguration("discopop").get("discopopPath");
 
-    public static buildPath: string = vscode.workspace.getConfiguration("discopop").get("path") + "/build";
+    public static buildPath: string = vscode.workspace.getConfiguration("discopop").get("discopopPath") + "/build";
 
-    public static fileMapperPath: string = vscode.workspace.getConfiguration("discopop").get("path") + "/scripts/dp-fmap";
+    public static fileMapperPath: string = vscode.workspace.getConfiguration("discopop").get("discopopPath") + "/scripts/dp-fmap";
     
     public static clangPath: string = vscode.workspace.getConfiguration("discopop").get("clangPath");
 }
